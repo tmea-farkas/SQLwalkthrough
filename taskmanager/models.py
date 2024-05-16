44d2f2327fc5bffd56fdfcb3136dd892e5d9c573
Timea Farkas <tmeafarkas@yahoo.com>
2024-05-16 17:07:15 +0000

    adding more stuff

---
+++ taskmanager/models.py
@@ -0,0 +1,26 @@
+from taskmanager import db
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique = True, nullable = False)
+    tasks = db.Relationship("Task", backref = "category", cascade = "all, delete", lazy = True)
+
+    def __repr__(self):
+        return self.category_name
+
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique = True,nullable = False)
+    task_description = db.Column(db.Text, nullable = False)
+    is_urgent = db.Column(db.Boolean, default = False, nullable = False)
+    due_date = db.Column(db.Date, nullable = False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), ondelete = "CASCADE", nullable = False)
+
+    def __repr__(self):
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
+
